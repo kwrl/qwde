042d9d548090110a49f04228c36d36a63917c78c
Håkon Kaurel <haakongk@stud.ntnu.no>
2018-01-16 21:04:11 +0100

    PystockStockPriceReader implemented

---
+++ qwde.pystock/src/main/java/qwde/pystock/PystockStockPrice.java
@@ -0,0 +1,50 @@
+package qwde.pystock;
+
+import java.sql.Timestamp;
+
+import qwde.models.StockPrice;
+
+public class PystockStockPrice implements StockPrice {
+	private final double highPrice;
+	private final double lowPrice;
+	private final String company;
+	private final Timestamp timestamp;
+
+	public PystockStockPrice(double highPrice, double lowPrice, String company, Timestamp timestamp) {
+		super();
+		this.highPrice = highPrice;
+		this.lowPrice = lowPrice;
+		this.company = company;
+		this.timestamp = timestamp;
+	}
+
+	public double getHighPrice() {
+		return highPrice;
+	}
+
+	public double getLowPrice() {
+		return lowPrice;
+	}
+
+	@Override
+	public double getPrice() {
+		return (highPrice+lowPrice)/2.0;
+	}
+
+	@Override
+	public String getCompany() {
+		return company;
+	}
+
+	@Override
+	public Timestamp getTimestamp() {
+		return timestamp;
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Override
+	public String toString() {
+		return company + " [" + timestamp.toGMTString() +"]: "+getPrice();
+	}
+
+}
--- qwde.pystock/src/main/java/qwde/pystock/PystockStockPriceReader.java
+++ qwde.pystock/src/main/java/qwde/pystock/PystockStockPriceReader.java
@@ -1,23 +1,94 @@
 package qwde.pystock;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
 
 import qwde.models.StockPrice;
 import qwde.util.StockPriceReader;
 
 public class PystockStockPriceReader implements StockPriceReader {
-	public PystockStockPriceReader(String directoryPath) {
-		File directory = new File(directoryPath);
-		for(File file : directory.listFiles()){
-			System.out.println(file.getName());
+	private final List<StockPrice> stockPrices;
+	
+	public PystockStockPriceReader(String directoryPath) throws IOException {
+		stockPrices = Files.walk(Paths.get(directoryPath))
+		.map(path -> path.toFile())
+		.filter(File::isFile)
+		.filter(file -> file.getName().endsWith(".tar.gz"))
+		.parallel()
+		.flatMap(PystockStockPriceReader::getPricesFromCompressedArchive)
+		.collect(Collectors.toList());
+	}
+
+	private static Stream<StockPrice> getPricesFromCompressedArchive(File compressedArchive) {
+		try {
+			TarArchiveInputStream stream = new TarArchiveInputStream(new GzipCompressorInputStream(new BufferedInputStream(new FileInputStream(compressedArchive))));
+			TarArchiveEntry entry;
+			while((entry = stream.getNextTarEntry()) != null) {
+				if("prices.csv".equals(entry.getName())) {
+					BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+					List<StockPrice> prices = reader.lines()
+							.map(line -> {
+								try {
+									return parseStockPrice(line);
+								} catch(Exception e) {
+									return null;
+								}
+							})
+							.filter(x -> x != null)
+							.collect(Collectors.toList());
+					
+					reader.close();
+					return prices.stream();
+				}
+			}
+			stream.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+		
+		return Stream.empty();
+	}
+	
+	private static StockPrice parseStockPrice(String line) {
+		String[] tokens = line.split(",");
+		return new PystockStockPrice(Double.parseDouble(tokens[3]), Double.parseDouble(tokens[4]), tokens[0], parseTimestamp(tokens[1]));
+	}
+	
+	@SuppressWarnings("deprecation")
+	private static Timestamp parseTimestamp(String line) {
+		String[] tokens = line.split("-");
+		return new Timestamp(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]), 0, 0, 0, 0);
 	}
 
 	public StockPrice read() {
-		return null;
+		if(stockPrices.isEmpty()) {
+			return null;
+		}
+		return stockPrices.remove(0);
 	}
 
-	public static void main(String[] args) {
-		new PystockStockPriceReader("C:\\Users\\Haakon Kaurel\\pystock-data");
+	public static void main(String[] args) throws IOException {
+		StockPriceReader reader = new PystockStockPriceReader("C:\\Users\\Haakon Kaurel\\pystock-data");
+		StockPrice price;
+		while((price = reader.read()) != null) {
+			System.out.println(price);
+		}
 	}
 }
