7a1afc942554303677b1b405764dc6e1a2f60b70
Anders Sildnes <andsild@posteo.net>
2019-07-28 00:48:56 +0200

    test webhook

--- main/src/main/java/qwde/servlets/IndexServlet.java
+++ main/src/main/java/qwde/servlets/IndexServlet.java
@@ -15,6 +15,6 @@ public class IndexServlet extends HttpServlet {
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     response.setContentType("text/html");
     response.setStatus(HttpServletResponse.SC_OK);
-    response.getWriter().println("<h1>if this text is shown, webhook works!</h1>"); 
+    response.getWriter().println("<h1>MARK 4</h1>"); 
   } 
 }
