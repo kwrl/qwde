437d62ac90f4fd14519ddc2bd55a30cbfd1a23b2
Anders Sildnes <andsild@posteo.net>
2019-08-03 23:21:40 +0200

    Bump to jdk11, make it possible to read all data for a date range

--- qwde.pystock/src/main/java/qwde/pystock/PystockStockPriceReader.java
+++ qwde.pystock/src/main/java/qwde/pystock/PystockStockPriceReader.java
@@ -13,6 +13,7 @@ import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -27,6 +28,7 @@ import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
 import org.apache.commons.io.FilenameUtils;
 
 import qwde.models.StockPrice;
+import qwde.pystock.PystockStockPrice;
 import qwde.util.StockPriceReader;
 
 public class PystockStockPriceReader implements StockPriceReader {
@@ -34,26 +36,56 @@ public class PystockStockPriceReader implements StockPriceReader {
   public final static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyyMMdd");
 
   private boolean fileNameMatchesDate(File file, Set<LocalDate> dates) {
-    LocalDate fileNameAsDate = LocalDate.parse(FilenameUtils.getBaseName(file.getName()), dateTimeFormatter);
+    LocalDate fileNameAsDate = LocalDate.parse(FilenameUtils.getBaseName(FilenameUtils.getBaseName(file.getName())), dateTimeFormatter);
     return dates.contains(fileNameAsDate);
   }
 
-  public PystockStockPriceReader(Set<LocalDate> desirableDates) throws IOException {
-    this.stockPrices = Files.walk(new File("../../..").toPath())
-            .map(path -> path.toFile())
-            .filter(File::isFile)
-            .filter(file -> file.getName().endsWith(".tar.gz"))
-            .filter(file -> fileNameMatchesDate(file, desirableDates))
-            .map(file -> {
-              try {
-                return new FileInputStream(file);
-              } catch (FileNotFoundException exception) {
-                throw new UncheckedIOException(exception);
-              }
-            })
-            .parallel()
-            .flatMap(PystockStockPriceReader::getPricesFromCompressedArchive)
-            .collect(Collectors.toList());
+  private static String getGitRootDirectory() throws IOException {
+    ProcessBuilder builder = new ProcessBuilder("git", "rev-parse", "--show-toplevel");
+    builder.redirectErrorStream(true);
+    Process process = builder.start();
+    InputStream is = process.getInputStream();
+    BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+
+    return reader.readLine();
+  }
+
+  private static Set<LocalDate> getDateRange(LocalDate startDate, LocalDate endDate) {
+    return startDate.datesUntil(endDate).filter(d -> !(d.getDayOfWeek() == DayOfWeek.SATURDAY || d.getDayOfWeek() == DayOfWeek.SUNDAY)).collect(Collectors.toSet());
+  }
+
+  public PystockStockPriceReader(LocalDate startDate, LocalDate endDate) throws IOException {
+    Set<LocalDate> desirableDates = getDateRange(startDate, endDate.plusDays(1));
+    Set<Integer> years = desirableDates.stream().map(date -> date.getYear()).collect(Collectors.toSet());
+    Stream<Path> yearPaths = years.stream()
+      .map(year -> {
+        try {
+          Path yearPath = Path.of(getGitRootDirectory(), "pystock-data",  year.toString());
+          return yearPath;
+        } catch (IOException exception) {
+          throw new UncheckedIOException(exception);
+        }
+      });
+    this.stockPrices = yearPaths.flatMap(yearPath -> {
+      try {
+        return Files.walk(yearPath)
+          .map(path -> path.toFile())
+          .filter(File::isFile)
+          .filter(file -> file.getName().endsWith(".tar.gz"))
+          .filter(file -> fileNameMatchesDate(file, desirableDates))
+          .map(file -> {
+            try {
+              return new FileInputStream(file);
+            } catch (FileNotFoundException exception) {
+              throw new UncheckedIOException(exception);
+            }
+          });
+      } catch (IOException exception) {
+        throw new UncheckedIOException(exception);
+      }})
+      .parallel()
+      .flatMap(PystockStockPriceReader::getPricesFromCompressedArchive)
+      .collect(Collectors.toList());
   }
 
   public PystockStockPriceReader(InputStream stockPriceFile) throws IOException {
@@ -65,14 +97,13 @@ public class PystockStockPriceReader implements StockPriceReader {
 
     ClassLoader classLoader = PystockStockPriceReader.class.getClassLoader();
     InputStream resourceStream = classLoader.getResourceAsStream(String.format("pystock-data/%s/%s.tar.gz",
-            date.format(yearFormatter),
-            date.format(PystockStockPriceReader.dateTimeFormatter)));
+          date.format(yearFormatter),
+          date.format(PystockStockPriceReader.dateTimeFormatter)));
     if (resourceStream == null) {
       throw new FileNotFoundException(String.format("Could not find file '%s'", date));
     }
 
-    return new PystockStockPriceReader(resourceStream);
-  }
+    return new PystockStockPriceReader(resourceStream);  }
 
   public static Stream<StockPrice> getPricesFromCompressedArchive(InputStream compressedArchive) {
     try {
@@ -82,15 +113,15 @@ public class PystockStockPriceReader implements StockPriceReader {
         if ("prices.csv".equals(entry.getName())) {
           BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
           List<StockPrice> prices = reader.lines()
-                  .map(line -> {
-                    try {
-                      return parseStockPrice(line);
-                    } catch (Exception e) {
-                      return null;
-                    }
-                  })
-                  .filter(x -> x != null)
-                  .collect(Collectors.toList());
+            .map(line -> {
+              try {
+                return parseStockPrice(line);
+              } catch (Exception e) {
+                return null;
+              }
+            })
+          .filter(x -> x != null)
+            .collect(Collectors.toList());
 
           reader.close();
           return prices.stream();
--- qwde.pystock/src/test/java/qwde/pystock/test/PystockStockPriceReaderTest.java
+++ qwde.pystock/src/test/java/qwde/pystock/test/PystockStockPriceReaderTest.java
@@ -3,26 +3,36 @@ package qwde.pystock.test;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import com.google.common.truth.Truth;
 
-import qwde.pystock.PystockStockPrice;
 import qwde.pystock.PystockStockPriceReader;
 
 public class PystockStockPriceReaderTest {
-    private PystockStockPriceReader getPystockPriceReader(LocalDate date) throws IOException {
-        ClassLoader classLoader = PystockStockPriceReader.class.getClassLoader();
-        InputStream resourceStream = classLoader.getResourceAsStream(date.format(PystockStockPriceReader.dateTimeFormatter) + ".tar.gz");
-        if (resourceStream == null) {
-            throw new FileNotFoundException(String.format("Could not find file '%s'", date));
-        }
-
-        return new PystockStockPriceReader(resourceStream);
+  private PystockStockPriceReader getPystockPriceReader(LocalDate date) throws IOException {
+    ClassLoader classLoader = PystockStockPriceReaderTest.class.getClassLoader();
+    InputStream resourceStream = classLoader.getResourceAsStream(date.format(PystockStockPriceReader.dateTimeFormatter) + ".tar.gz");
+    if (resourceStream == null) {
+      throw new FileNotFoundException(String.format("Could not find file '%s'", date));
     }
+
+    return new PystockStockPriceReader(resourceStream);
+  }
+
+  @Test
+  public void readSpecificTickerInfo() throws IOException {
+    PystockStockPriceReader pystockStockPriceReader = new PystockStockPriceReader(LocalDate.of(2017, 01, 02), LocalDate.of(2017, 01, 02));
+
+    BigDecimal twitterPrice = pystockStockPriceReader.read().stream().filter(x -> x.getCompany().equals("TWTR")).map(x -> x.getPrice()).findFirst().get();
+    
+    Truth.assertThat(twitterPrice).isEqualToIgnoringScale("16.3949995");
+  }
+
   @Test
   public void testRead() throws IOException {
     PystockStockPriceReader pyReader = getPystockPriceReader(LocalDate.of(2017, 01, 02));
