8946ec07c65d8fd4562dd5eb7dcc4c7966e4542d
Thomas H. Evang <thomasevang@gmail.com>
2019-08-14 21:34:56 +0200

    Multiple moving averages

--- main/src/main/java/qwde/plotly/LinePlotRenderer.java
+++ main/src/main/java/qwde/plotly/LinePlotRenderer.java
@@ -4,15 +4,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import tech.tablesaw.api.DoubleColumn;
 import tech.tablesaw.api.Table;
-import tech.tablesaw.plotly.Plot;
 import tech.tablesaw.plotly.components.Axis;
 import tech.tablesaw.plotly.components.Figure;
 import tech.tablesaw.plotly.components.Layout;
 import tech.tablesaw.plotly.components.Page;
+import tech.tablesaw.plotly.traces.BarTrace;
 import tech.tablesaw.plotly.traces.ScatterTrace;
-import weka.gui.explorer.VisualizePanel.ScatterDefaults;
 
 import java.util.stream.IntStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import qwde.ml.MovingAverage;
 
@@ -27,19 +28,29 @@ public class LinePlotRenderer {
         DoubleColumn xColumn = DoubleColumn.create("xcol", xData);
         DoubleColumn yColumn = DoubleColumn.create("ycol", data);
 
-        int window = 10;
-        Double[] sma = MovingAverage.simpleMovingAverage(data, window);
-        DoubleColumn yColumn2 = DoubleColumn.create("sma", sma);
+        List<ScatterTrace> smaPlots = new ArrayList<>();
+
+        for (int window = 10; window <= 100; window += 10) {
+            Double[] sma = MovingAverage.simpleMovingAverage(data, window);
+            DoubleColumn smaColumn = DoubleColumn.create("sma", sma);
+
+            Table smaTable = Table.create("smaTable").addColumns(xColumn, smaColumn);
+            
+            ScatterTrace smaTrace = ScatterTrace.builder(smaTable.nCol("xcol"), smaTable.nCol("sma")).mode(ScatterTrace.Mode.LINE)
+            .name("Window Length: " + window)
+            .build(); 
+
+            smaPlots.add(smaTrace);
+        }
 
         Table table = Table.create("table").addColumns(xColumn, yColumn);
-        Table table2 = Table.create("table 2").addColumns(xColumn, yColumn2);
 
-        ScatterTrace trace = ScatterTrace.builder(table.nCol("xcol"), table.nCol("ycol"))
+        ScatterTrace dataPlot = ScatterTrace.builder(table.nCol("xcol"), table.nCol("ycol"))
                 .mode(ScatterTrace.Mode.LINE)
+                .opacity(0.5)
+                .name("Price")
                 .build();
         
-        ScatterTrace trace2 = ScatterTrace.builder(table2.nCol("xcol"), table2.nCol("sma")).mode(ScatterTrace.Mode.LINE).build();
-                        
         Axis xAxis = Axis.builder()
                 .title("Time")
                 .autoRange(Axis.AutoRange.TRUE)
@@ -47,17 +58,23 @@ public class LinePlotRenderer {
 
         Axis yAxis = Axis.builder()
                 .title("Price")
+                .autoRange(Axis.AutoRange.TRUE)
                 .build();
 
         Layout layout = Layout.builder()
                 .title("Stock Prices")
                 .xAxis(xAxis)
                 .yAxis(yAxis)
+                .width(1920)
+                .height(1080)
                 .build();
 
-        Figure figure = new Figure(layout, trace, trace2);
+        smaPlots.add(dataPlot);
 
+        Figure figure = new Figure(layout, smaPlots.toArray(new ScatterTrace[smaPlots.size()]));
+    
         Page page = Page.pageBuilder(figure, "testdiv").build();
+
         return page.asJavascript();
     }
 }
